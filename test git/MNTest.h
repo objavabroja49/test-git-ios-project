6b072952dada9d40cf598864d5817ea641884d6d
Mirjana Novakovic <mirjananovakovic@Mirjanas-MacBook-Air.local>
2014-05-17 17:34:49 +0200

    added test class

---
+++ test git/MNTest.h
@@ -0,0 +1,13 @@
+//
+//  MNTest.h
+//  test git
+//
+//  Created by Mirjana Novakovic on 5/17/14.
+//  Copyright (c) 2014 Mirjana Novakovic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MNTest : NSObject
+
+@end
---
+++ test git/MNTest.m
@@ -0,0 +1,13 @@
+//
+//  MNTest.m
+//  test git
+//
+//  Created by Mirjana Novakovic on 5/17/14.
+//  Copyright (c) 2014 Mirjana Novakovic. All rights reserved.
+//
+
+#import "MNTest.h"
+
+@implementation MNTest
+
+@end
